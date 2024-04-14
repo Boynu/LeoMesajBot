8660efc2d2e6c443a40f45cbfa36efe5a39b45a6
Ferid Abusov <122831032+Theferid@users.noreply.github.com>
2024-04-15 01:54:00 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 26090016 #your API_ID
 API_HASH = "5b842f9801712684f2b98d70ead6538d" #yourAPI_HASH
-TOKEN = "" #your bot token
-MONGODB_URI = "" #https://www.mongodb.com
+TOKEN = "7137607506:AAFnt0HE3JS75Af-ENvEVw7tnKixYgKPrgE" #your bot token
+MONGODB_URI = "mongodb+srv://fridabisov26:feridabusov2@cluster0.rrmqdo9.mongodb.net/?retryWrites=true&w=majority" #https://www.mongodb.com
 WHISPER_ICON_URL = "https://graph.org/file/16afae7eb8b4e1641aeac.png"
 SUDO_USERS = [864254084, 686505963]
 BOT_USERNAME = "@whispersx_bot" #your bot user name as @username
