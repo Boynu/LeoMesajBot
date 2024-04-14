b8f6060274e079912b2dc681aa08659359c9fb1f
Ferid Abusov <122831032+Theferid@users.noreply.github.com>
2024-04-15 01:56:04 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ API_HASH = "5b842f9801712684f2b98d70ead6538d" #yourAPI_HASH
 TOKEN = "7137607506:AAFnt0HE3JS75Af-ENvEVw7tnKixYgKPrgE" #your bot token
 MONGODB_URI = "mongodb+srv://fridabisov26:feridabusov2@cluster0.rrmqdo9.mongodb.net/?retryWrites=true&w=majority" #https://www.mongodb.com
 WHISPER_ICON_URL = "https://graph.org/file/16afae7eb8b4e1641aeac.png"
-SUDO_USERS = [864254084, 686505963]
-BOT_USERNAME = "@whispersx_bot" #your bot user name as @username
-CHAT_ID = #channel id to log new users
+SUDO_USERS = [6997801235]
+BOT_USERNAME = "@leomesajbot" #your bot user name as @username
+CHAT_ID = -1002143361911 #channel id to log new users
