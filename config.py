761b341dbdbc82b323220a80d3f6983f122db311
Ferid Abusov <122831032+Theferid@users.noreply.github.com>
2024-04-15 01:53:17 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_ID = #your API_ID
-API_HASH = "" #yourAPI_HASH
+API_ID = 26090016 #your API_ID
+API_HASH = "5b842f9801712684f2b98d70ead6538d" #yourAPI_HASH
 TOKEN = "" #your bot token
 MONGODB_URI = "" #https://www.mongodb.com
 WHISPER_ICON_URL = "https://graph.org/file/16afae7eb8b4e1641aeac.png"
