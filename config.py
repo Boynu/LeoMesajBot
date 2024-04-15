6f0626db88f4c1910f50f9f054512fe9f1775119
Ferid Abusov <122831032+Theferid@users.noreply.github.com>
2024-04-15 19:11:41 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ API_ID = 26090016 #your API_ID
 API_HASH = "5b842f9801712684f2b98d70ead6538d" #yourAPI_HASH
 TOKEN = "7137607506:AAFnt0HE3JS75Af-ENvEVw7tnKixYgKPrgE" #your bot token
 MONGODB_URI = "mongodb+srv://fridabisov26:feridabusov2@cluster0.rrmqdo9.mongodb.net/?retryWrites=true&w=majority" #https://www.mongodb.com
-WHISPER_ICON_URL = "https://graph.org/file/16afae7eb8b4e1641aeac.png"
+WHISPER_ICON_URL = "https://telegra.ph/file/ef638e3317ca21f1fd479.jpg"
 SUDO_USERS = [6997801235]
 BOT_USERNAME = "@leomesajbot" #your bot user name as @username
 CHAT_ID = -1002056467668 #channel id to log new users
