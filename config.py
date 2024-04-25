c0e02a371f48a1c3e5ccdab936124144e0fbdbbc
Boynu <139995517+Boynu@users.noreply.github.com>
2024-04-25 11:31:16 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 API_ID = 26090016 #your API_ID
 API_HASH = "5b842f9801712684f2b98d70ead6538d" #yourAPI_HASH
-TOKEN = "7137607506:AAFnt0HE3JS75Af-ENvEVw7tnKixYgKPrgE" #your bot token
+TOKEN = "5855945910:AAGG4VBeaissEzVSYiLyX47zwQiJkkSolLQ" #your bot token
 MONGODB_URI = "mongodb+srv://fridabisov26:feridabusov2@cluster0.rrmqdo9.mongodb.net/?retryWrites=true&w=majority" #https://www.mongodb.com
 WHISPER_ICON_URL = "https://telegra.ph/file/ef638e3317ca21f1fd479.jpg"
-SUDO_USERS = [6997801235]
-BOT_USERNAME = "@leomesajbot" #your bot user name as @username
-CHAT_ID = -1002056467668 #channel id to log new users
+SUDO_USERS = [5663585448]
+BOT_USERNAME = "@sesizKOLGE_816_bot" #your bot user name as @username
+CHAT_ID = -1001872296360 #channel id to log new users
